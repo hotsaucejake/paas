e68090973e78a51dcf0cf400760dd9197d921135
hotsaucejake <get@hotsaucejake.com>
2018-10-24 11:40:19 -0400

    roles and permissions support
    - Reason(s):
    
    - Change(s):
    
    - Reference(s):
      - https://github.com/spatie/laravel-permission

--- app/User.php
+++ app/User.php
@@ -5,14 +5,17 @@ namespace App;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-
+// passwordless login
 use dam1r89\PasswordlessAuth\UsersRepository;
 use dam1r89\PasswordlessAuth\Contracts\UsersProvider;
+// roles and permissions
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable implements UsersProvider
 {
     use Notifiable;
     use UsersRepository;
+    use HasRoles;
 
     /**
      * The attributes that are mass assignable.

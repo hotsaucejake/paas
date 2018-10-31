b47b4223c8a4ed0579dad95a60a96553a4dae323
hotsaucejake <get@hotsaucejake.com>
2018-10-31 02:14:05 -0400

    dashboard view
    - Reason(s):
    
    - Change(s):
    
    - Reference(s):

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\PermanentPlacement;
+use App\ContractBilling;
 
 class DashboardController extends Controller
 {
@@ -25,7 +27,12 @@ class DashboardController extends Controller
      */
     public function index()
     {
-        return view('monster.dashboard.index');
+        $pp_all = PermanentPlacement::all()->count();
+        $pp_approved = PermanentPlacement::approved()->count();
+        $cb_all = ContractBilling::all()->count();
+        $cb_approved = ContractBilling::approved()->count();
+
+        return view('monster.dashboard.index', compact('pp_all', 'pp_approved', 'cb_all', 'cb_approved'));
     }
 
 }
--- resources/views/monster/dashboard/index.blade.php
+++ resources/views/monster/dashboard/index.blade.php
@@ -26,6 +26,56 @@ Dashboard
 
 @section('content')
 
+<div class="row">
+
+    <div class="col-sm">
+        <div class="social-widget">
+            <div class="soc-header box-facebook">
+                <h3 class="text-light">Permanent Placements</h3>
+                <i class="fa fa-plus-circle"></i>
+            </div>
+            <div class="soc-content">
+                <div class="col-4 b-r">
+                    <h3 class="font-medium">{{ $pp_approved }}</h3>
+                    <h5 class="text-muted">Approved</h5>
+                </div>
+                <div class="col-4 b-r">
+                    <h3 class="font-medium">{{ $pp_all - $pp_approved }}</h3>
+                    <h5 class="text-muted">Needs Approval</h5>
+                </div>
+                <div class="col-4">
+                    <h3 class="font-medium">{{ $pp_all }}</h3>
+                    <h5 class="text-muted">Total</h5>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-sm">
+        <div class="social-widget">
+            <div class="soc-header box-twitter">
+                <h3 class="text-light">Contractor Requests</h3>
+                <i class="fa fa-copy"></i>
+            </div>
+            <div class="soc-content">
+                <div class="col-4 b-r">
+                    <h3 class="font-medium">{{ $cb_approved }}</h3>
+                    <h5 class="text-muted">Approved</h5>
+                </div>
+                <div class="col-4 b-r">
+                    <h3 class="font-medium">{{ $cb_all - $cb_approved }}</h3>
+                    <h5 class="text-muted">Needs Approval</h5>
+                </div>
+                <div class="col-4">
+                    <h3 class="font-medium">{{ $cb_all }}</h3>
+                    <h5 class="text-muted">Total</h5>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</div>
+
 @endsection
 
 

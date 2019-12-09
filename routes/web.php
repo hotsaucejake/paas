d822a810f8057de6df4c379b9826a91501f997c8
hotsaucejake <get@hotsaucejake.com>
2019-12-08 23:42:06 -0500

    active status for permanent placements

--- routes/web.php
+++ routes/web.php
@@ -33,6 +33,7 @@ Route::middleware('verified')->prefix('dashboard')->group(function () {
     Route::resource('permanent_placement', 'PermanentPlacementController');
     Route::patch('permanent_placement/{permanent_placement}/approve', 'PermanentPlacementController@approve')->name('permanent_placement.approve');
     Route::patch('permanent_placement/{permanent_placement}/unapprove', 'PermanentPlacementController@unapprove')->name('permanent_placement.unapprove');
+    Route::patch('permanent_placement/{permanent_placement}/activeStatus', 'PermanentPlacementController@activeStatus')->name('permanent_placement.activeStatus');
 
     Route::get('contract_billing/export', 'ContractBillingController@export')->name('contract_billing.export');
     Route::resource('contract_billing', 'ContractBillingController');
